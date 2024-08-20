ea2a643d4a1c304548bd69f7fcd1a1d933872af4
Ben Noble <ben.noble@madetech.com>
2024-08-20 14:21:06 +0100

    Can't reach a node we're not linked to

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 class Node{
     canReach(other){
-        return false;
+        return other == this;
     }
 }
 
--- node.test.js
+++ node.test.js
@@ -3,6 +3,12 @@ const {Node} = require("./node");
 describe ('Nodes....', () => {
     test('Node can reach itself', () => {
         var g = new Node();
-        expect(g.canReach(g)).toBe(false);
+        expect(g.canReach(g)).toBe(true);
+    });
+
+    test('Node can reach itself', () => {
+        var g = new Node();
+        var a = new Node();
+        expect(g.canReach(a)).toBe(false);
     });
 })
