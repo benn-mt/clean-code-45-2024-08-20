317becd7b4bc845566e7aa12e519437539dfd9b1
Ben Noble <ben.noble@madetech.com>
2024-08-20 10:33:30 +0100

    Added area behaviour

--- rectangle.test.js
+++ rectangle.test.js
@@ -1,14 +1,12 @@
 const {Rectangle} = require("./rectangle");
 
 describe("Rectangles...", () =>{
-    test("Have equal area is length and width are same", () => {
-        expect(new Rectangle(1,3).area()).toEqual(new Rectangle(1,3).area());
-        expect(new Rectangle(2,4).area()).not.toEqual(new Rectangle(1,3).area());
-    });
-
     test("Simple area calculations", () => {
         expect(new Rectangle(1,3).area()).toEqual(3);
         expect(new Rectangle(2,4).area()).toEqual(8);
         expect(new Rectangle(4,3).area()).toEqual(12);
+        expect(new Rectangle(12,0).area()).toEqual(0);
+        expect(new Rectangle(0,6).area()).toEqual(0);
+        expect(new Rectangle(0,0).area()).toEqual(0);
     });
 })
