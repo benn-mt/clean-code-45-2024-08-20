23f85e92023bf955f0f997bea783f8e5ec4e5cf2
Ben Noble <ben.noble@madetech.com>
2024-08-21 10:35:19 +0100

    Refactored canReach to use _pathTo logic

--- node.js
+++ node.js
@@ -6,25 +6,11 @@ class Node{
     }
 
     canReach(destination){
-        var visitedNodes = [];
-        return this._canReach(destination, visitedNodes);
-    }
-
-    _canReach(destination, visitedNodes){
-        if (destination === this){
-            return true;
-        }
-        if (visitedNodes.includes(this)){
+        var visitedNodes = this._pathTo(destination, []);
+        if (visitedNodes == Node.UNREACHABLE) {
             return false;
         }
-        visitedNodes.push(this);
-        for (var i = 0; i < this._children.length; i++){
-            var child = this._children[i];
-            if (child._canReach(destination, visitedNodes)){
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 
     hopCount(destination){
